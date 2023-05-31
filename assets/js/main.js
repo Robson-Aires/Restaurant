30541cd66f2536441299755104d1838a1b6fa933
Robson Aires <robsonaires@outlook.com.br>
2023-05-31 14:32:55 -0300

    home section finalizada

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/*=============== SHOW MENU ===============*/
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+/*=============== HOME SWIPER ===============*/
+
+let homeSswiper = new Swiper('.home-swiper', {
+    spaceBetween: 30,
+    loop: 'true',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+/*=============== DEALS TAB ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
