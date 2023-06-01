70bd396f4eeeac408e6cc01f6f929f23499a57eb
Robson Aires <robsonaires@outlook.com.br>
2023-06-01 12:04:33 -0300

    javascript do deals section finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,5 +25,26 @@ let homeSswiper = new Swiper('.home-swiper', {
 });
 
 /*=============== DEALS TAB ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContent = document.querySelectorAll('[content]');
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+        console.log(target);
+    tabContent.forEach((tabContent) => {
+     tabContent.classList.remove('active-tab');
+ });
+
+
+     target.classList.add('active-tab');
+
+    tabs.forEach((tab) => {
+        tab.classList.remove('active-tab');
+    });
+
+    tab.classList.add('active-tab');
+ });
+ });
 
 /*=============== SHOW SCROLL UP ===============*/
